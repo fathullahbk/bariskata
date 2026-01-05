1ddcb0d111e709e6b183bc3a27aed18a4a15a98c
fathullahbk <fathullah05@guru.smp.belajar.id>
2026-01-06 07:57:31 +0800

    perbaikan mode dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning> {/* Tambahkan suppressHydrationWarning */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
