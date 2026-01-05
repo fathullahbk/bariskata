79fd758b2a4a3242d1f3ff5bdfd205c4b5866b2b
fathullahbk <fathullah05@guru.smp.belajar.id>
2026-01-05 09:52:43 +0800

    Initial commit

---
+++ blog/tes/artikel-tes.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <article className="max-w-2xl mx-auto py-10">
+      <h1 className="text-3xl font-bold bg-blue-500 text-white p-2">Halo Dunia!</h1>
+      <p className="mt-4 text-gray-700">
+        Ini adalah artikel pertama saya yang dibuat manual di Next.js menggunakan Tailwind CSS.
+      </p>
+    </article>
+  )
+}
